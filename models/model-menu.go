46d9dcc2b9899ab4be2ac3c83268bfc515a8f141
Viky Arifiansyah <vikyarifiansyah@gmail.com>
2025-08-21 17:48:08 +0700

    form-submit-validator

--- models/model-menu.go
+++ models/model-menu.go
@@ -11,8 +11,8 @@ func GetMenu() []Menu {
 	menus = append(menus, Menu{Label: "Dashboard", Href: "/", Icon: "home"})
 	menus = append(menus, Menu{Label: "Workspaces", Href: "/workspaces", Icon: "briefcase-business"})
 	// menus = append(menus, Menu{Label: "Workspaces", Href: "/workspaces", Icon: "monitor-check"})
-	menus = append(menus, Menu{Label: "My Tasks", Href: "/tasks", Icon: "list-checks"})
-	menus = append(menus, Menu{Label: "Members", Href: "/tasks", Icon: "users"})
+	menus = append(menus, Menu{Label: "My Tasks", Href: "/tasks", Icon: "layout-list"})
+	menus = append(menus, Menu{Label: "Members", Href: "/members", Icon: "users"})
 	menus = append(menus, Menu{Label: "Achieved", Href: "/achieve", Icon: "badge-check"})
 	menus = append(menus, Menu{Label: "Settings", Href: "/achieve", Icon: "settings"})
 
